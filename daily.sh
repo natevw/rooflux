f0aa8dab1a4e6be1fa31f24fc21b6627da71b967
Nathan Vander Wilt <nate@calftrail.com>
2013-09-25 22:01:21 -0700

    add usage notes to daily script

--- daily.sh
+++ daily.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
 
+# === Usage ===
+#
+# Put this into `crontab -e`:
+#
+#     55 23 * * * rooflux/daily.sh >> inverter_daily.log
+#
+# (Make sure your system timezone is inverter-local.)
+
 curl -XPOST 192.168.1.49 -d '{"jsonrpc":"2.0","method":"GetPowerLog","params":[17,255,"'`date +%Y-%m-%d`' 00:00:00","Wh","15min",96],"id":0}'
 
