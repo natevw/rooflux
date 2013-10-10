e0be9b4c4c30aa6853ec90068644344889a3b1be
Nathan Vander Wilt <nate@calftrail.com>
2013-10-09 22:50:37 -0700

    display AC in watts instead of amps

--- transmit.node.js
+++ transmit.node.js
@@ -8,7 +8,8 @@ var CALL = {
     method:"GeteNexusData",
     params:[
         {"path":"eNEXUS_0006[s:255,t:17]","datatype":"INT16U"},     // DC voltage * 10
-        {"path":"eNEXUS_0008[s:255,t:17]","datatype":"INT16U"},     // AC current * 1000
+        //{"path":"eNEXUS_0008[s:255,t:17]","datatype":"INT16U"},     // AC current * 1000
+        {"path":"eNEXUS_0010[s:255,t:17]","datatype":"INT16U"},     // AC power
         {"path":"eNEXUS_0043[s:255,t:17,n:4]","datatype":"INT32U"}  // total energy * 1000
     ], id:0
 };
@@ -26,6 +27,8 @@ inverter.rpc.post(CALL, function (e,d) {
                 obj['DC input'] = (+res.value/10).toFixed(1) + "V"; break;
             case '0008':
                 obj['AC output'] = (+res.value/1000).toFixed(1) + "A"; break;
+            case '0010':
+                obj['AC output'] = (+res.value).toFixed(1) + "W"; break;
             case '0043':
                 obj['Total produced'] = (+res.value/1000).toFixed(1) + "kW"; break;
         }
